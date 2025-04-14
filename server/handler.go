86ddcf8cd94a9103fb9e1c07a5498e3570c4f4bc
hneemann <helmut.neemann@arcor.de>
2025-04-14 21:08:14 +0200

    fixed line ending issue

--- server/handler.go
+++ server/handler.go
@@ -46,6 +46,13 @@ func ReadExamples() []Example {
 		panic(err)
 	}
 
+	for i := range examples.Examples {
+		c := examples.Examples[i].Code
+		strings.ReplaceAll(c, "\r", "")
+		strings.ReplaceAll(c, "\n", "\r\n")
+		examples.Examples[i].Code = c
+	}
+
 	log.Printf("loaded %d examples", len(examples.Examples))
 
 	return examples.Examples
