da62a52f127afc7f3b4ecffae6b066225236b2ed
hneemann <helmut.neemann@arcor.de>
2025-05-13 09:55:49 +0200

    adds CSS to the second window

--- server/templates/runInBrowser.js
+++ server/templates/runInBrowser.js
@@ -11,10 +11,20 @@ let myWindow;
 function runSourceInWindow() {
     let source = document.getElementById('source');
 
-    if (!(myWindow && !myWindow.closed)) {
+    if (myWindow && !myWindow.closed) {
+        myWindow.document.body.innerHTML = "";
+    } else {
         myWindow = window.open("", "", "width=810,height=620");
     }
 
-    myWindow.document.body.innerHTML = generateOutput(source.value);
+    myWindow.document.write("<head>\n" +
+        "  <meta charset=\"UTF-8\">\n" +
+        "  <title>Control</title>\n" +
+        "  <link rel=\"icon\" type=\"image/svg\" href=\"/assets/icon.svg\">\n" +
+        "  <link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/main.css\"/>\n" +
+        "</head>\n" +
+        "<body>\n");
+    myWindow.document.write(generateOutput(source.value));
+    myWindow.document.write("\n</body>\n</html>");
     source.focus();
 }
--- server/templates/runOnServer.js
+++ server/templates/runOnServer.js
@@ -20,7 +20,15 @@ function runSourceInWindow() {
             myWindow = window.open("", "", "width=810,height=620");
         }
 
+        myWindow.document.write("<head>\n" +
+            "  <meta charset=\"UTF-8\">\n" +
+            "  <title>Control</title>\n" +
+            "  <link rel=\"icon\" type=\"image/svg\" href=\"/assets/icon.svg\">\n" +
+            "  <link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/main.css\"/>\n" +
+            "</head>\n" +
+            "<body>\n");
         myWindow.document.write(a);
+        myWindow.document.write("\n</body>\n</html>");
         source.focus();
     });
 }
