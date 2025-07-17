79c05312838913c82db85fea6c150b2f3500cdf5
hneemann <helmut.neemann@arcor.de>
2025-07-17 09:23:35 +0200

    improved formatting of numbers in logarithmic scaling

--- graph/scaling.go
+++ graph/scaling.go
@@ -3,6 +3,7 @@ package graph
 import (
 	"fmt"
 	"math"
+	"strings"
 	"time"
 )
 
@@ -167,13 +168,70 @@ func createLogTicks(logMin, parentMin, parentMax float64, tr func(v float64) flo
 			}
 			if tv >= parentMin {
 				if i == 1 {
-					t.Label = fmt.Sprintf("%g", f)
+					t.Label = FormatFloat(f)
 				}
 				ticks = append(ticks, t)
 			}
 		}
 		m++
 	}
+}
+
+func FormatFloat(v float64) string {
+	s := fmt.Sprintf("%g", v)
+	if !strings.ContainsRune(s, 'e') {
+		return s
+	}
+
+	va := math.Abs(v)
+	log := int(math.Floor(math.Log10(va)))
+	val := fmt.Sprintf("%.4g", va/exp10(log))
+	s = getExpStr(log)
+	if val != "1" {
+		s = val + "⋅" + s
+	}
+	if v < 0 {
+		s = "-" + s
+	}
+	return s
+}
+
+func getExpStr(log int) string {
+	l := log
+	if log < 0 {
+		l = -log
+	}
+	s := ""
+	for l > 0 {
+		dig := l % 10
+		l /= 10
+		switch dig {
+		case 0:
+			s = "⁰" + s
+		case 1:
+			s = "¹" + s
+		case 2:
+			s = "²" + s
+		case 3:
+			s = "³" + s
+		case 4:
+			s = "⁴" + s
+		case 5:
+			s = "⁵" + s
+		case 6:
+			s = "⁶" + s
+		case 7:
+			s = "⁷" + s
+		case 8:
+			s = "⁸" + s
+		case 9:
+			s = "⁹" + s
+		}
+	}
+	if log < 0 {
+		s = "⁻" + s
+	}
+	return "10" + s
 }
 
 // CreateFixedStepAxis creates an axis with a fixed step size.
--- graph/scaling_test.go
+++ graph/scaling_test.go
@@ -1,7 +1,9 @@
 package graph
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"math"
 	"testing"
 )
 
@@ -27,6 +29,59 @@ func Test_Axis(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			_, ti, _, _ := LinearAxis(tt.min, tt.max, tt.bounds, tt.ctw, 0.02)
 			assert.EqualValues(t, tt.want, ti)
+		})
+	}
+}
+
+func TestFormatFloat(t *testing.T) {
+	tests := []struct {
+		v    float64
+		want string
+	}{
+		{0, "0"},
+		{1e0, "1"},
+		{1e1, "10"},
+		{1e2, "100"},
+		{1e3, "1000"},
+		{1e4, "10000"},
+		{1e5, "100000"},
+		{1e6, "10⁶"},
+		{1e7, "10⁷"},
+		{1e8, "10⁸"},
+		{1e9, "10⁹"},
+		{1e10, "10¹⁰"},
+		{1e11, "10¹¹"},
+		{3e0, "3"},
+		{3e1, "30"},
+		{3e2, "300"},
+		{3e3, "3000"},
+		{3e4, "30000"},
+		{3e5, "300000"},
+		{3e6, "3⋅10⁶"},
+		{3e7, "3⋅10⁷"},
+		{3e8, "3⋅10⁸"},
+		{3e9, "3⋅10⁹"},
+		{3e10, "3⋅10¹⁰"},
+		{3e11, "3⋅10¹¹"},
+		{math.Pi * 1e7, "3.142⋅10⁷"},
+		{-3e3, "-3000"},
+		{-3e7, "-3⋅10⁷"},
+		{3e-3, "0.003"},
+		{5e-7, "5⋅10⁻⁷"},
+		{1e-1, "0.1"},
+		{1e-2, "0.01"},
+		{1e-3, "0.001"},
+		{1e-4, "0.0001"},
+		{1e-5, "10⁻⁵"},
+		{3.2e-5, "3.2⋅10⁻⁵"},
+		{1e-6, "10⁻⁶"},
+		{1e-7, "10⁻⁷"},
+		{1e-8, "10⁻⁸"},
+		{1e-11, "10⁻¹¹"},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.v), func(t *testing.T) {
+			assert.Equalf(t, tt.want, FormatFloat(tt.v), "FormatFloat(%v)", tt.v)
 		})
 	}
 }
